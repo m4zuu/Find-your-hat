942a4562db006ad48237f437cf4bde3b58737cc9
Mauricio <mauriciozmeng@gmail.com>
2024-03-24 07:22:04 -0600

    creating field

--- main.js
+++ main.js
@@ -6,7 +6,25 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class hatGame {
-    constructor(){
+    constructor(field){
+        this.field = field
         
     }
+
+    newField(){
+        let field = [ ]
+        let rows = prompt('How many rows the field will be? ')
+        let col = prompt('How many cells the field will be? ')
+
+        for (let i = 0 ; i <= rows ; i++){
+            field.push(fieldCharacter)
+            for(let j = 0; j<=col; j++){
+                field.push(fieldCharacter)
+            }
+            return field
+        }
+    }
 }
+
+let newGame = new hatGame()
+newGame.newField()
